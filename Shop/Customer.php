36957f2671b12e3520dd2d3e74966be72e48e03e
Kalina Mariia <46577545+kalina-iva@users.noreply.github.com>
2019-11-07 15:53:29 +0500

    Update Customer.php

--- Shop/Customer.php
+++ Shop/Customer.php
@@ -5,18 +5,18 @@ include 'Kassir.php';
 
 class Customer
 {
-    public $number_of_goods;									//сколько покупатель набрал продуктов
+	public $number_of_goods;					//сколько покупатель набрал продуктов
     
-    function __construct($number)
-    {
-        $this->number_of_goods = $number;
-    }
+	function __construct($number)
+	{
+		$this->number_of_goods = $number;
+	}
 	
 	function initialize(&$kassir, $customer)
 	{
 		if($kassir)	
 		{
-			usort($kassir, function($kassir1,$kassir2)			//массив касс сортируется по возрастанию кол-ва чел. в очереди
+			usort($kassir, function($kassir1,$kassir2)	//массив касс сортируется по возрастанию кол-ва чел. в очереди
 			  {
 				  if(count($kassir1->queue) == count($kassir2->queue)) return 0;
 				  return (count($kassir1->queue) > count($kassir2->queue)) ? 1 : -1;
@@ -35,7 +35,7 @@ class Customer
 		   !$kassir)
 		{
 			$kassa = new Kassir($customer);
-    		$kassir[] = $kassa;						
+    			$kassir[] = $kassa;						
 		}
 	}
 }
